b41ddca5629ebddd80497f4ab4482f40ae51b523
spiridovichp <4hgqcddv28@privaterelay.appleid.com>
2026-02-24 13:19:51 +0300

    мсправление ошибок

--- backend/src/api/services/storageService.js
+++ backend/src/api/services/storageService.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const storageConfig = require('../../config/storage');
 const archiver = require('archiver');
-const Storage = require('../../db/models/Storage');
+// ...existing code...
 
 /**
  * StorageService
