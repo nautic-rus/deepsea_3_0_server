ddd1a056326599d3cdc719196ad242f389efcf7a
spiridovichp <4hgqcddv28@privaterelay.appleid.com>
2026-05-21 11:41:45 +0300

    обновленеи метода спецификации убъдаление лишних полей

--- backend/src/api/services/specificationsService.js
+++ backend/src/api/services/specificationsService.js
@@ -31,17 +31,6 @@ class SpecificationsService {
       id: v.id,
       specification_id: v.specification_id,
       version: v.version,
-      notes: v.notes,
-      created_by: v.created_by,
-      created_by_first_name: v.created_by_first_name,
-      created_by_last_name: v.created_by_last_name,
-      created_by_middle_name: v.created_by_middle_name,
-      created_by_full_name: [v.created_by_last_name, v.created_by_first_name, v.created_by_middle_name].filter(Boolean).join(' ') || null,
-      updated_by: v.updated_by,
-      updated_by_first_name: v.updated_by_first_name,
-      updated_by_last_name: v.updated_by_last_name,
-      updated_by_middle_name: v.updated_by_middle_name,
-      updated_by_full_name: [v.updated_by_last_name, v.updated_by_first_name, v.updated_by_middle_name].filter(Boolean).join(' ') || null,
       created_at: v.created_at,
       updated_at: v.updated_at
     }));
